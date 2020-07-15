7ae1e1b882ca8b61b64978e4ad1b429a09bf4801
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-07-15 16:25:15 -0700

    add youtube tutorial

--- website/.cache/sync-requires.js
+++ website/.cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/gabrielajardimaquino/Desktop/ReactimeONTEST/reactime/website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/gabrielajardimaquino/Desktop/ReactimeONTEST/reactime/website/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/gabrielajardimaquino/Desktop/ReactimeONTEST/reactime/website/src/pages/index.js")))
 }
